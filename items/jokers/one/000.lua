f8ece170c7fea4609a8fb9f4b2c7ad62cbd0d7b1
FireIce421 <92722498+FireIce421@users.noreply.github.com>
2025-12-08 19:56:27 +0100

    attempt different 000 effect implementation

--- items/jokers/one/000.lua
+++ items/jokers/one/000.lua
@@ -6,23 +6,14 @@ SMODS.Joker {
     cost = 6, -- dummy price, update later
     rarity = 1,
     calculate = function(self, card, context)
-        if context.buying_card and context.card.config.center.key == self.key and context.cardarea == G.jokers  then
-            return {
-                func = function()
-                    card_eval_status_text(context.blueprint_card or card, 'extra', nil, nil, nil, {message = "+"..tostring(1).." Joker Slot", colour = G.C.DARK_EDITION})
-                    G.jokers.config.card_limit = G.jokers.config.card_limit + card.ability.extra.joker_slots
-                    return true
-                end
-            }
-        end
-        if context.selling_self  then
-            return {
-                func = function()
-                    card_eval_status_text(context.blueprint_card or card, 'extra', nil, nil, nil, {message = "-"..tostring(1).." Joker Slot", colour = G.C.RED})
-                    G.jokers.config.card_limit = math.max(1, G.jokers.config.card_limit - card.ability.extra.joker_slots)
-                    return true
-                end
-            }
-        end
+
+    end,
+
+    add_to_deck = function(self, card, from_debuff)
+        G.jokers.config.card_limit = G.jokers.config.card_limit + card.ability.extra.joker_slots
+    end,
+    
+    remove_from_deck = function(self, card, from_debuff)
+        G.jokers.config.card_limit = G.jokers.config.card_limit - card.ability.extra.joker_slots
     end
 }
